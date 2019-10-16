92cee19b8a93e8446acaef508d7e6bfad26aaec6
Aakrit Subedi <aakritsubedi9@gmail.com>
2019-10-17 00:54:49 +0545

    Img Dir changed

--- js/bird.js
+++ js/bird.js
@@ -20,7 +20,7 @@ class Bird{
         birdDiv.style.position='absolute';
         birdDiv.style.left=this.birdX+'px';
         birdDiv.style.top=this.birdY+'px';
-        birdDiv.style.backgroundImage='url(\' ../images/bluebird-upflap.png \')';
+        birdDiv.style.backgroundImage='url(\' images/bluebird-upflap.png \')';
 
         this.bird= this.wrapper.appendChild(birdDiv);
         this.goDown();
